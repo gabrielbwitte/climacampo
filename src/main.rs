3c0a1feaa11e9157c1684dc72529f1bbc418a9f5
gabriel <gabrielbwitte@gmail.com>
2025-06-24 16:08:25 -0400

    cria env para port e ip, e modifica github yaml

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,24 @@ use api::user_api::{
     get_all_users,
 };
 use database::mongodb_data::MongoData;
+use std::env;
+use dotenv::dotenv;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let addrs: String = match env::var("ADDRS") {
+            Ok(v) => v.to_string(),
+            Err(_) => format!("Error: não foi possivel ler o env ADDRS"),
+        };
+    let var_port = match env::var("PORT") {
+            Ok(v) => v.to_string(),
+            Err(_) => format!("Error: não foi possivel ler o env ADDRS"),
+        };
+
+    let port: u16 = var_port.parse().expect("Error de tipo da env PORT");
+    
+    println!("Start climacampo");
     let db = MongoData::init().await;
     let db_data = Data::new(db);
 
@@ -26,7 +41,7 @@ async fn main() -> std::io::Result<()> {
             .service(delete_user)
             .service(get_all_users)
     })
-    .bind(("localhost", 8080))?
+    .bind((addrs.as_str(), port))?
     .run()
     .await
 }
